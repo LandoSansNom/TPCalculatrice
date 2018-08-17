f5c1bae3f3bbdcf7a2b9ab3e650aa3ad05c2d4ad
LandoSansNom <cherlanphilippe@gmail.com>
2018-08-17 10:25:17 -0400

    Display screen finished

--- src/tpcalculatrice/Window.java
+++ src/tpcalculatrice/Window.java
@@ -9,7 +9,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -30,11 +29,10 @@ public class Window extends JFrame {
             
             JPanel container = new JPanel();
             JPanel panEcran = new JPanel();
-            
+            Border border = BorderFactory.createLineBorder(Color.black);
             // Create JLabel with border for display screen
             JLabel label = new JLabel("0");
-            Border border = BorderFactory.createLineBorder(Color.BLACK);
-            label.setPreferredSize(new Dimension(220,30));
+            label.setPreferredSize(new Dimension(220,20));
             label.setBorder(border);
             label.setHorizontalAlignment(JLabel.RIGHT);
             panEcran.setPreferredSize(new Dimension(220,30));
